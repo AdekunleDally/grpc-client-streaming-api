c9eff40ac9e551c9ee8583556a37b466b0fbf93f
AdekunleDally <adekunledally@gmail.com>
2024-04-07 21:31:31 +0100

    Setup client to run on port 8082
    - Configure client to listen on port 8082
    - Update documentation with new port information

--- averagecalculator/client/main.go
+++ averagecalculator/client/main.go
@@ -1,5 +1,19 @@
 package main
 
 import (
+	"log"
+
 	pb "github.com/AdekunleDally/grpc-client-streaming-api/averagecalculator/proto"
-)  
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+var addr string = "localhost:8082"
+
+func main() {
+	conn, err := grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Failed to connect : %v\n", err)
+	}
+	c := pb.NewAverageServiceClient(conn)
+}
